10652290a59f92612a6819cebf49fc5b5e31e9c7
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-11-03 22:39:59 -0600

    escribiendo en state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,32 @@
 import { categories } from "../data/categories";
+import { useState } from "react";
 
 export default function Form() {
+  const [activity, setActivity] = useState({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (e)=>{
+    console.log(e.target)
+    setActivity({
+        ... activity,
+        [e.target.id]:e.target.value
+    })
+  }
+
   return (
     <form className=" space-y-5 bg-white shadow p-10 rounded-lg">
       <div className=" grid grid-cols-1 gap-3">
         <label htmlFor="category" className=" font-bold">
-          Categoria:{" "}
+          Categoria:
         </label>
         <select
-          name=""
+          value={activity.category}
           id="category"
           className=" border border-slate-300 p-2 rounded-lg w-full bg-white"
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -21,14 +37,16 @@ export default function Form() {
       </div>
 
       <div className=" grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className=" font-bold">
+        <label htmlFor="name" className=" font-bold">
           Actividad:
         </label>
         <input
-          id="activity"
+          id="name"
           type="text"
           className=" border border-slate-300 p-w rounded-lg"
           placeholder="Ej. Comida, jugo de naranja, ensalada, ejercicio, pesas, bicicleta"
+          value={activity.name}
+          onChange={handleChange}
         ></input>
       </div>
 
@@ -41,12 +59,15 @@ export default function Form() {
           type="number"
           className=" border border-slate-300 p-w rounded-lg"
           placeholder="Calorias. ej. 300 o 500"
+          value={activity.calories}
+          onChange={handleChange}
         ></input>
       </div>
 
-      <input type="submit" 
-      className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-      value={"Guardar comida o guardar ejercicio"}
+      <input
+        type="submit"
+        className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value={"Guardar comida o guardar ejercicio"}
       />
     </form>
   );
