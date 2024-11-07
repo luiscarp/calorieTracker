4c7623881bc046c66376f29a719dcf1150f01a52
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-11-07 11:40:31 -0600

    mostrando actividades y background

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,10 +1,18 @@
 import { Activity } from "../types";
+import { categories } from "../data/categories";
+import { useMemo } from "react";
 
 type ActivityListProps = {
   activities: Activity[];
 };
 
 export default function ActivityList({ activities }: ActivityListProps) {
+    const categoryName = useMemo(
+        () => (
+            category : Activity["category"]) => 
+                categories.map( (cat) => cat.id === category ? cat.name:"")
+        , [activities])
+
   return (
     <>
       <h2 className=" text-4xl font-bold text-slate-600 text-center">
@@ -17,7 +25,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
           className=" px-5 py-10 bg-white mt-5 flex justify-between"
         >
           <div className=" space-y-2 relative">
-            <p>{activity.category}</p>
+            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"}`}>{categoryName(+activity.category)}</p>
 
             <p className=" text-2xl font-bold pt-5">{activity.name}</p>
 
