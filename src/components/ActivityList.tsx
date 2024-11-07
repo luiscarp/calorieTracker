f97d78028df8ea51f30c2132e462573cf234dfc5
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-11-07 11:48:20 -0600

    icono editar en actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { useMemo } from "react";
+import { PencilSquareIcon } from "@heroicons/react/16/solid";
 
 type ActivityListProps = {
   activities: Activity[];
@@ -35,7 +36,13 @@ export default function ActivityList({ activities }: ActivityListProps) {
             </p>
           </div>
 
-          <div></div>
+          <div className=" flex gap-5 items-center">
+            <button>
+                <PencilSquareIcon className=" h-8 w-8 text-gray-800"></PencilSquareIcon>
+            </button>
+
+
+          </div>
         </div>
       ))}
     </>
