cd9f2d13fc0465fa47deb64acc4472f655690feb
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-11-08 10:36:35 -0600

    boton de eliminado de actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Activity } from "../types";
 import { categories } from "../data/categories";
 import { useMemo, Dispatch } from "react";
-import { PencilSquareIcon } from "@heroicons/react/16/solid";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/16/solid";
 import { ActivityActions } from "../reducers/activity-reducer";
 
 type ActivityListProps = {
@@ -9,12 +9,15 @@ type ActivityListProps = {
   dispatch: Dispatch<ActivityActions>;
 };
 
-export default function ActivityList({ activities, dispatch }: ActivityListProps) {
-    const categoryName = useMemo(
-        () => (
-            category : Activity["category"]) => 
-                categories.map( (cat) => cat.id === category ? cat.name:"")
-        , [activities])
+export default function ActivityList({
+  activities,
+  dispatch,
+}: ActivityListProps) {
+  const categoryName = useMemo(
+    () => (category: Activity["category"]) =>
+      categories.map((cat) => (cat.id === category ? cat.name : "")),
+    [activities]
+  );
 
   return (
     <>
@@ -28,7 +31,13 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
           className=" px-5 py-10 bg-white mt-5 flex justify-between"
         >
           <div className=" space-y-2 relative">
-            <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"}`}>{categoryName(+activity.category)}</p>
+            <p
+              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${
+                activity.category === 1 ? "bg-lime-500" : "bg-orange-500"
+              }`}
+            >
+              {categoryName(+activity.category)}
+            </p>
 
             <p className=" text-2xl font-bold pt-5">{activity.name}</p>
 
@@ -40,9 +49,19 @@ export default function ActivityList({ activities, dispatch }: ActivityListProps
 
           <div className=" flex gap-5 items-center">
             <button
-            onClick={()=> dispatch({type:"set-activeId", payload:{id:activity.id}})}
+              onClick={() =>
+                dispatch({ type: "set-activeId", payload: { id: activity.id } })
+              }
             >
-                <PencilSquareIcon className=" h-8 w-8 text-gray-800"></PencilSquareIcon>
+              <PencilSquareIcon className=" h-8 w-8 text-gray-800"></PencilSquareIcon>
+            </button>
+
+            <button
+              onClick={() =>
+                dispatch({ type: "delete-activity", payload: { id: activity.id } })
+              }
+            >
+              <XCircleIcon className=" h-8 w-8 text-red-500"></XCircleIcon>
             </button>
 
 
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,8 @@ import { Activity } from "../types";
 
 export type ActivityActions =
   | { type: "save-activity"; payload: { newActivity: Activity } }
-  | { type: "set-activeId"; payload: { id: Activity["id"] } };
+  | { type: "set-activeId"; payload: { id: Activity["id"] } }
+  | { type: "delete-activity"; payload: { id: Activity["id"] } }
 
 export type ActivityState = {
   activities: Activity[];
@@ -42,5 +43,15 @@ export const activityReducer = (
       activeId: action.payload.id,
     };
   }
+
+  if (action.type === "delete-activity") {
+
+    return {
+        ...state,
+        activities: state.activities.filter( activity => activity.id !== action.payload.id)
+        
+    }
+  }
   return state;
 };
+
